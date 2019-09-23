87257c214a7ba3dfed6f61aa11593ee587412154
Samuel Blin <sblin@MacBook-Pro-de-Samuel-Xebialabs.local>
2019-09-23 17:17:52 +0200

    Initial commit

--- src/main/resources/redmine/Server.py
+++ src/main/resources/redmine/Server.py
@@ -7,6 +7,29 @@
 #
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
-#from redminelib import Redmine
+from org.apache.http.client import ClientProtocolException
 
-#redmine = Redmine(configuration.url, key=configuration.apiKey)
+params = {'url': configuration.url, 'proxyHost': configuration.proxyHost, 'proxyPort': configuration.proxyPort, 'proxyUsername': configuration.proxyUsername, 'proxyPassword': configuration.proxyPassword}
+
+response = None
+
+try:
+    headers = None
+    if configuration.apiKey:
+        headers = {'X-Redmine-API-Key': configuration.apiKey }
+    elif configuration.password:
+        params['username'] = configuration.username
+        params['password'] = configuration.password
+    response = HttpRequest(params).get('/issues.json',
+                                       contentType='application/json', headers=headers)
+except ClientProtocolException:
+    raise Exception("URL is not valid")
+
+# Redmine api returns 403 in case you are authenticated but not enough permissions
+if response.status != 403 and response.status != 200:
+    reason = "Unknown"
+    if response.status == 400:
+        reason = "Bad request"
+    elif response.status == 401:
+        reason = "Unauthorized"
+    raise Exception("HTTP response code %s, reason %s" % (response.status, reason))
