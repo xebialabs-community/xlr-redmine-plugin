f3a5343eb552e2ebb4dbeadbab076760c6105068
Samuel Blin <sblin@mbpdesaebialabs.home>
2019-09-24 22:14:04 +0200

    Added task

--- src/main/resources/redmine/__init__.py
+++ src/main/resources/redmine/__init__.py
@@ -7,3 +7,61 @@
 #
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
+import com.xhaus.jyson.JysonCodec as Json
+from util import error
+from xlrelease.HttpRequest import HttpRequest
+from org.apache.http.client import ClientProtocolException
+
+class RedmineServer:
+
+    def __init__(self, redmine_server):
+        if redmine_server is None:
+            error(u'No Server provided')
+        self.redmine_server = redmine_server
+
+    def getIssues(self, issueIds=None, projectId=None):
+        request = self._createRequest()
+        issues = {}
+        filters = ''
+        url = '/issues.json'
+
+        if issueIds != None:
+            filters = 'issue_id=' + issueIds
+        
+        if projectId != None:
+            if len(filters) > 0:
+                filters += '&'
+            filters += 'project_id=' + projectId
+
+        if len(filters) > 0:
+            url += '?' + filters
+
+        try:
+            response = request.get(url, content=None, contentType='application/json', headers=self._createHeaders())
+            if response.status == 200:
+                data = Json.loads(response.response)
+                for item in data['issues']:
+                    id = item['id']
+                    subject = item['subject']
+                    issues[id] = subject
+                return issues
+            else:
+                error(u'Failed to get issues', response)
+        except ClientProtocolException:
+            raise Exception()
+        
+        return issues
+
+
+    def _createRequest(self):
+        params = self.redmine_server.copy()
+        if params['apiKey']:
+            params.pop('username')
+            params.pop('password')
+        return HttpRequest(params)
+
+    def _createHeaders(self):
+        headers = None
+        if self.redmine_server['apiKey']:
+            headers = {'X-Redmine-API-Key': self.redmine_server['apiKey'] }
+        return headers
--- src/main/resources/redmine/util.py
+++ src/main/resources/redmine/util.py
@@ -8,44 +8,10 @@
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
 
-
-import com.xhaus.jyson.JysonCodec as json
-
-if redmineServer is None:
-    print "No server provided."
-    sys.exit(1)
-  
-
-ISSUE_CREATED_STATUS = 201
-
-redmineUrl = redmineServer['url']
-redmineAPIKey = redmineServer['apiKey']
-
-content="""
-{
-  "issue": {
-    "project_id": %s,
-    "subject": "%s",
-    "priority_id": %s
-  }
-}""" % (projectId, subject, priorityId)
-
-headers = {'X-Redmine-API-Key': redmineAPIKey }
-
-request = HttpRequest(redmineServer)
-response = request.post('/issues.json', content, contentType = 'application/json', headers = headers)
-
-if response.status == ISSUE_CREATED_STATUS:
-
-    print response.status
-    print response.response
-    print response.headers
-
-    if response.headers['Content-Type'].startswith('application/json'):
-        data = json.loads(response.response)
-        issueId = data.get('issues.id')
-        print "Created issue %s in Redmine at %s." % (issueId, redmineUrl)
-else:
-    print "Failed to create issue in Redmine at %s." % redmineUrl
-    response.errorDump()
-    sys.exit(1)
+def error(text, response=None):
+    print
+    print u"#### Error: {0} ".format(text)
+    if response is not None:
+        response.errorDump()
+    print
+    raise Exception(text)
